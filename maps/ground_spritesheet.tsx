6e81e01fe83d140dd52191053534f81829713487
Tsurugi21 <90088023+Tsurugi21@users.noreply.github.com>
2022-02-15 21:33:36 +0100

    uploaded maps

---
+++ maps/ground_spritesheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="ground_spritesheet" tilewidth="32" tileheight="32" tilecount="30" columns="6">
+ <image source="../img/ground_spritesheet.png" width="192" height="160"/>
+</tileset>
